96be9e526f31cbd70d6c840b37b13aa03a34bf9a
yerinirey <yerin886@naver.com>
2024-01-28 00:57:58 +0900

    11.16 Upload Photo

--- medias/models.py
+++ medias/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from common.models import CommonModel
 
 class Photo(CommonModel):
-    file = models.ImageField()
+    file = models.URLField()
     description = models.CharField(max_length=140,)
     room = models.ForeignKey("rooms.Room",
                             null=True, blank=True,
@@ -16,7 +16,7 @@ class Photo(CommonModel):
         return "Photo File"
 
 class Video(CommonModel):
-    file = models.FileField()
+    file = models.URLField()
     experience = models.OneToOneField("experiences.Experience",
                                       on_delete=models.CASCADE,
                                       related_name="videos",)
--- rooms/views.py
+++ rooms/views.py
@@ -8,6 +8,7 @@ from .models import Amenity, Room
 from categories.models import Category
 from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerializer
 from reviews.serializers import ReviewSerializer
+from medias.serializers import PhotoSerializer
 class Amenities(APIView):
     def get(self, request):
         all_amenities = Amenity.objects.all()
@@ -199,7 +200,24 @@ class RoomAmenities(APIView):
 
 class RoomPhotos(APIView):
 
-    # def get_object
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
 
     def post(self, request, pk):
-        pass
+        room = self.get_object(pk)
+        if not request.user.is_authenticated:
+            raise NotAuthenticated
+        if request.user != room.owner:
+            raise PermissionDenied
+        
+        serializer = PhotoSerializer(data=request.data)
+        if serializer.is_valid():
+            photo = serializer.save(room=room)
+            serializer = PhotoSerializer(photo)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
