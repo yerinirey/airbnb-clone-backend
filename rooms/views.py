00b42e34eb3fba523735b3cf7e223b7e6b1ced43
yerinirey <yerin886@naver.com>
2024-01-27 23:46:33 +0900

    task-Amenities Pagination

--- rooms/views.py
+++ rooms/views.py
@@ -171,10 +171,27 @@ class RoomReviews(APIView):
             page = int(request.query_params.get('page', 1))
         except ValueError:
             page = 1
+        room = self.get_object(pk)
         page_size = 10
         start = (page - 1) * page_size
         end = start + page_size
-        room = self.get_object(pk)
         serializer = ReviewSerializer(room.reviews.all()[start:end], many=True)
         return Response(serializer.data)
-        
+
+class RoomAmenities(APIView):
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+    def get(self, request, pk):
+        try:
+            page = int(request.query_params.get('page', 1))
+        except ValueError:
+            page = 1
+        room = self.get_object(pk)
+        page_size = 10
+        start = (page - 1) * page_size
+        end = start + page_size
+        serializer = AmenitySerializer(room.amenities.all()[start:end], many=True)
+        return Response(serializer.data)
